2fb795034e21ee5582604dafa253f9d1fa404e2c
Hossam Tuwano <hossamtuwano@gmail.com>
2022-06-26 15:57:38 +0300

    :construction :chore add newPwd component

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,64 @@
 import "./App.css";
-import AddHostel from "./pages/hostel/AddHostel";
+
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Signup from "./pages/Auth/Signup";
-import Login from "./pages/Auth/Login";
 import Header from "./components/Header/Header";
 import ViewHostel from "./pages/hostel/ViewHostel";
 import UpdateHostel from "./pages/hostel/UpdateHostel";
 import SearchPage from "./pages/hostel/SearchPage";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 import { PrivateRoute, LoginPrivateRoute } from "./components/PrivateRoute";
 import ManageHostels from "./pages/hostel/hostelManagement/ManageHostels";
-import Location from "./components/Location";
+
+import { Provider } from "react-redux";
+
+import store from "./store";
+
+import LoginRedux from "./pages/Auth/LoginRedux";
+import Reset from "./pages/Auth/Reset";
+import NewPassword from "./pages/Auth/NewPassword";
 
 function App() {
   const [token, setToken] = useState();
   const [success, setSuccess] = useState();
 
-  useEffect(() => {
-    setToken(localStorage.getItem("token"));
-    setSuccess(localStorage.getItem("success"));
-  }, [token]);
-
   return (
-    // <store.Provider store>
-    <div className="App">
-      {/* <Hostels /> */}
-      <Header success={success} />
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Provider store={store}>
+      <div className="App bg-[#f1f5f9]">
+        {/* <Hostels /> */}
+        <Header success={success} />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        <Route
-          path="/manage-hostel"
-          element={
-            <PrivateRoute token={token}>
-              <ManageHostels />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/update-hostel" element={<UpdateHostel />} />
+          <Route
+            path="/manage-hostel"
+            element={
+              <PrivateRoute token={token}>
+                <ManageHostels />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/update-hostel" element={<UpdateHostel />} />
 
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Login" element={<LoginRedux />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/reset/:token" element={<NewPassword />} />
 
-        <Route
-          path="/View-hostel"
-          element={
-            <LoginPrivateRoute token={token}>
-              <ViewHostel />
-            </LoginPrivateRoute>
-          }
-        />
-        <Route path="/search-page" element={<SearchPage />} />
-        <Route path="/location" element={<Location />} />
-      </Routes>
-    </div>
-    // </store.Provider>
+          <Route
+            path="/View-hostel"
+            element={
+              <LoginPrivateRoute token={token}>
+                <ViewHostel />
+              </LoginPrivateRoute>
+            }
+          />
+          <Route path="/search-page" element={<SearchPage />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
 
