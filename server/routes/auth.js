6ec1e3b28aef0aa1ba25c211e7c0d505fbec9611
Hossam Tuwano <hossamtuwano@gmail.com>
2022-06-26 15:56:49 +0300

    :construction :chore added new_pwd route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,5 +8,7 @@ router.post("/signup", authController.signup);
 router.post("/role", roleController.role);
 router.post("/login", authController.login);
 router.get("/users", authController.get_user);
+router.post("/reset", authController.post_reset);
+router.post("/new_password", authController.new_password);
 
 module.exports = router;
